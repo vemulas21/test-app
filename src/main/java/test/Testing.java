32b421442dde3ba73b7e4a85acad83741cf18655
srikanth <srikanth@srikanth-PC>
2016-12-29 23:55:33 +0530

    for loop implemented

--- src/main/java/test/Testing.java
+++ src/main/java/test/Testing.java
@@ -5,5 +5,9 @@ public class Testing {
 	public static void main(String[] args) {
 		System.out.println("Helo");
 		System.out.println("Helo");
+		for(int i = 0; i<5; i++) {
+			System.out.println("loop executing : " + i);
+		}
 	}
+	
 }
