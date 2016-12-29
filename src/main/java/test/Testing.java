e54a85de450ed820b5ef0615d7abe816680135f2
srikanth <srikanth@srikanth-PC>
2016-12-29 23:55:50 +0530

    condition check implemented

--- src/main/java/test/Testing.java
+++ src/main/java/test/Testing.java
@@ -5,5 +5,10 @@ public class Testing {
 	public static void main(String[] args) {
 		System.out.println("Helo");
 		System.out.println("Helo");
+		int a = 2;
+		if(a == 2) {
+			System.out.println("Condition check");			
+		}
+		System.out.println("Done");
 	}
 }
